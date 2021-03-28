3dbcb6c47150c0402028ca67113d959b9d56178c
prabhu <prghere@gmail.com>
2021-03-28 03:50:07 -0400

    first commit

---
+++ src/test/java/com/erdossoftware/chat/talk/TalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdossoftware.chat.talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
